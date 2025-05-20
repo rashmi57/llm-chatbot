8144726585a3ec9e9069348a7b0f59c3cb91db09
rashmi57 <139947261+rashmi57@users.noreply.github.com>
2025-05-20 16:14:54 +0530

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,8 @@
+import PyPDF2
+
+def extract_text_from_pdf(uploaded_file):
+    text = ""
+    reader = PyPDF2.PdfReader(uploaded_file)
+    for page in reader.pages:
+        text += page.extract_text()
+    return text
